bd921325cc278fad271c9110b137b5c7a391957c
Ben Chatelain <ben@octop.ad>
2022-02-01 19:52:43 -0700

    🔥 Remove watchOS and tvOS platforms

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WebView",
     platforms: [
-      .iOS(.v13), .watchOS(.v6), .tvOS(.v13), .macOS(.v10_15)
+      .iOS(.v13), .macOS(.v10_15)
     ],
     products: [
         .library(
