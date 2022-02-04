e0c53acd3e3ce494aa980f00ec82e034dd2a93ad
Ben Chatelain <ben@octop.ad>
2022-02-03 20:23:32 -0700

    🍏 Add example iOS app

---
+++ WebViewApp/WebViewApp/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  WebViewApp
+//
+//  Created by Ben Chatelain on 2/3/22.
+//
+
+import WebView
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var webViewStore = WebViewStore()
+
+    var body: some View {
+        NavigationView {
+            WebView(webView: webViewStore.webView)
+                .navigationBarTitle(Text(verbatim: webViewStore.webView.title ?? ""),
+                                    displayMode: .inline)
+                .navigationBarItems(trailing: HStack {
+                    Button(action: goBack) {
+                        Image(systemName: "chevron.left")
+                            .imageScale(.large)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 32, height: 32)
+                    }.disabled(!webViewStore.webView.canGoBack)
+                    Button(action: goForward) {
+                        Image(systemName: "chevron.right")
+                            .imageScale(.large)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 32, height: 32)
+                    }.disabled(!webViewStore.webView.canGoForward)
+                })
+        }.onAppear {
+            self.webViewStore.webView.load(URLRequest(url: URL(string: "https://apple.com")!))
+        }
+    }
+
+    func goBack() {
+        webViewStore.webView.goBack()
+    }
+
+    func goForward() {
+        webViewStore.webView.goForward()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebViewApp/WebViewApp/WebViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewAppApp.swift
+//  WebViewApp
+//
+//  Created by Ben Chatelain on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
