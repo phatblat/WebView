c146c0ccfd5df3fdbee039c62e87be7570e02428
Ben Chatelain <ben@octop.ad>
2022-02-03 20:25:44 -0700

    ✅ Add test

--- Tests/WebViewTests/WebViewTests.swift
+++ Tests/WebViewTests/WebViewTests.swift
@@ -2,7 +2,16 @@
 import XCTest
 
 final class WebViewTests: XCTestCase {
-    func testExample() {
+    func testWebViewExists() {
         XCTAssertNotNil(WebView().webView)
+    }
+
+    func testWebViewLoading() {
+        let url = URL(string: "about:blank")!
+        let request = URLRequest(url: url)
+        let navigation = WebView().webView.load(request)
+        
+        XCTAssertNotNil(navigation)
+        XCTAssertEqual(navigation!.effectiveContentMode, .recommended)
     }
 }
