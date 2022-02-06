e92f295d9c75441897c0e6a62aa8d7a9f3a1050f
Ben Chatelain <ben@octop.ad>
2022-02-06 13:23:20 -0700

    💥 Rename update and scrollTo parameters

--- Sources/WebView/WebViewStore.swift
+++ Sources/WebView/WebViewStore.swift
@@ -1,28 +1,44 @@
 import WebKit
 import Combine
 
+/// Provides access to webview data.
 public class WebViewStore: ObservableObject {
     private var observers = Set<NSKeyValueObservation>()
 
-    @Published public var webView: WKWebView {
+    @Published
+    public var webView: WKWebView {
         didSet {
             setupObservers()
         }
     }
 
+    /// Initializes a new WebView.
+    /// - Parameter webView: Optional custom webview. Default WKWebView will be constructed if not provided.
     public init(webView: WKWebView = .init()) {
         self.webView = webView
 
         #if DEBUG
         self.webView
-          .configuration
-          .preferences
-          .setValue(true, forKey: "developerExtrasEnabled")
+            .configuration
+            .preferences
+            .setValue(true, forKey: "developerExtrasEnabled")
         #endif
 
         setupObservers()
     }
 
+    deinit {
+        observers.forEach {
+            // Not even sure if this is required?
+            // Probably wont be needed in future betas?
+            $0.invalidate()
+        }
+    }
+}
+
+/// Private methods
+fileprivate extension WebViewStore {
+    /// Description
     private func setupObservers() {
         func subscriber<Value>(for keyPath: KeyPath<WKWebView, Value>) -> NSKeyValueObservation {
             return webView.observe(keyPath, options: [.prior]) { _, change in
@@ -44,29 +60,30 @@ public class WebViewStore: ObservableObject {
             subscriber(for: \.canGoForward)
         ]
     }
-
-    deinit {
-        observers.forEach {
-            // Not even sure if this is required?
-            // Probably wont be needed in future betas?
-            $0.invalidate()
-        }
-    }
 }
 
+/// DOM manipulation and scrolling.
 extension WebViewStore {
-    func update(_ HTML: String, baseURL: URL) {
+    /// Loads a new document and scrolls to the same Y offset.
+    /// - Parameters:
+    ///   - html: HTML string of the new document.
+    ///   - baseURL: Optional base URL.
+    func update(_ html: String, baseURL: URL? = nil) {
         webView.evaluateJavaScript("window.pageYOffset") { [weak self] object, error in
-            self?.webView.loadHTMLString(HTML, baseURL: baseURL)
+            guard let strongSelf = self else { return }
+
+            strongSelf.webView.loadHTMLString(html, baseURL: baseURL)
 
             if let offset = object as? Int {
-                self?.scrollTo(offset)
+                strongSelf.scrollTo(offset)
             }
         }
     }
 
-    private func scrollTo(_ YOffset: Int) {
-        let script = "window.scrollTo(0, \(YOffset));"
+    /// Instructs the webview to scroll the viewport.
+    /// - Parameter yOffset: Vertical offset to scroll to.
+    private func scrollTo(_ yOffset: Int) {
+        let script = "window.scrollTo(0, \(yOffset));"
         let scrollScript = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         webView.configuration.userContentController.addUserScript(scrollScript)
     }
